35c972e8d086298f65eca64416801ff78ec28b84
bayupras141 <aebayu789@gmail.com>
2022-04-25 10:17:11 +0700

    perubahan syntaks conversi json

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -1,19 +1,17 @@
 class Movie{
-  int id;
-  String title;
-  double voteAvarage;
-  String overview;
-  String posterPath;
+  late int id;
+  late String title;
+  late double voteAverage;
+  late String overview;
+  late String posterPath;
 
-  Movie(this.id, this.title, this.voteAvarage, this.overview, this.posterPath);
+  Movie(this.id, this.title, this.voteAverage, this.overview, this.posterPath);
 
-  // conversi json
-  Movie.fromJson(Map<String, dynamic> json)
-    : id = json['id'],
-      title = json['title'],
-      voteAvarage = json['vote_average'] * 1.0,
-      overview = json['overview'],
-      posterPath = json['poster_path'];
-
-
+  Movie.fromJson(Map<String, dynamic> parsedJson) {
+    id = parsedJson['id'];
+    title = parsedJson['title'];
+    voteAverage = parsedJson['vote_average'] * 1.0;
+    overview = parsedJson['overview'];
+    posterPath = parsedJson['poster_path'];
+  }
 }
